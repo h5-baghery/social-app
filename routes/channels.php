21048a460d2cff58fc10f7afcac8ce8bc57885ba
Hassan Bagheri <h5.bagheri@gmail.com>
2025-06-08 22:52:35 +0330

    hale moshkele daryafte payam dar chat

--- routes/channels.php
+++ routes/channels.php
@@ -17,6 +17,6 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('chatchannel', function ($user, $id) {
+Broadcast::channel('chatchannel', function () {
     return auth()->check();
 });
